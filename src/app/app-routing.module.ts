f21705c17d03885d640c7b4be38262e84468bc5e
ToddEmonster <s.raharivelo@outlook.fr>
2020-02-14 11:49:42 +0100

    "login guard created and working. user interface just created"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { LoginGuard } from './core/guards/login.guard';
 
 
 const routes: Routes = [
@@ -14,8 +15,10 @@ const routes: Routes = [
     path:'home',
     component: HomeComponent
   },
-  {path:'login',
-  component: LoginComponent
+  {
+    path:'login',
+    component: LoginComponent,
+    canActivate: [LoginGuard]
   },
   {
     path:'**',
