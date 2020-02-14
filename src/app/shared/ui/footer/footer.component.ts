d6ce8e3f3b644c8b4685e5506c1e0ea8502f0eff
ToddEmonster <s.raharivelo@outlook.fr>
2020-02-14 14:47:58 +0100

    "now the footer shows if the user is connected"

--- src/app/shared/ui/footer/footer.component.ts
+++ src/app/shared/ui/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/services/user.service';
+import { UserInterface } from 'src/app/core/models/user-interface';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  public user: UserInterface;
 
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.userService.userSubject$.subscribe((user: UserInterface) => {
+      this.user = user;
+    });
   }
 
 }
