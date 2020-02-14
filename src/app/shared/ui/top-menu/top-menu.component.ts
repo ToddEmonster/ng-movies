cac1d55dfb54688531c462a758ce013a0d045f9e
ToddEmonster <s.raharivelo@outlook.fr>
2020-02-14 14:38:18 +0100

    "it works with the can activate and stuff"

--- src/app/shared/ui/top-menu/top-menu.component.ts
+++ src/app/shared/ui/top-menu/top-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/core/services/user.service';
+import { UserInterface } from 'src/app/core/models/user-interface';
 
 @Component({
   selector: 'app-top-menu',
@@ -7,12 +8,16 @@ import { UserService } from 'src/app/core/services/user.service';
   styleUrls: ['./top-menu.component.scss']
 })
 export class TopMenuComponent implements OnInit {
+  public user: UserInterface;
 
   constructor(
     public userService: UserService
   ) { }
 
   ngOnInit(): void {
+    this.userService.userSubject$.subscribe((user: UserInterface) => {
+      this.user = user;
+    });
   }
 
   public doLogout(): void {
