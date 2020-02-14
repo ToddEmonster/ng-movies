86bfc38097a798e2153edbf98132c6e84cf17022
ToddEmonster <s.raharivelo@outlook.fr>
2020-02-14 09:50:15 +0100

    "login/logout with isAuthenticated works"

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private _registeredUsers: any[];
+  public isAuthenticated: boolean = false;
+
+  constructor() {
+    this._registeredUsers = new Array<any>();
+    this._registeredUsers.push(
+      {
+        login: 'jlaubert',
+        password: 'totototo'
+      }
+    );
+  }
+
+  public authenticate(user: any): boolean {
+    const registeredUser: any = this._registeredUsers.find(
+      (obj: any) => obj.login == user.login && obj.password == user.password
+    );
+    if (registeredUser !== undefined) {
+      localStorage.setItem(
+        'user',
+        JSON.stringify(user)
+      ); 
+      this.isAuthenticated=true;
+      return true;
+    } 
+    return false;
+  }
+
+
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,9 @@ export class LoginComponent implements OnInit {
   public loginForm: FormGroup
   
   constructor(
-    private formBuilder: FormBuilder) { }
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userservice: UserService) { }
 
   public get login(): AbstractControl {
     return this.loginForm.controls.login;
@@ -40,7 +44,17 @@ export class LoginComponent implements OnInit {
 
 
   public doLogin(): void {
-    console.log('This is when the user gets logged in.')
+    // Local persistence
+    if (this.userservice.authenticate(this.loginForm.value)) {
+      // Road to home
+    this.router.navigate(['home']);
+    } else {
+      // TODO : some snackbar to keep user informed
+      // TODO : redraw form with empty values
+      this.login.setValue('');
+      this.password.setValue('');
+    };
+
   }
 
 }
