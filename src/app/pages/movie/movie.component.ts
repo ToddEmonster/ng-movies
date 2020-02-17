70e6f81d598a6d58916559556091524913c81b30
ToddEmonster <s.raharivelo@outlook.fr>
2020-02-17 13:53:46 +0100

    "In order to change the synopsis, the elements are in place, I just have to connect it to the back"

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from 'src/app/core/services/movie.service';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-movie',
@@ -9,11 +10,13 @@ import { MovieService } from 'src/app/core/services/movie.service';
 })
 export class MovieComponent implements OnInit {
   public movie: any;
+  public synopsisForm: FormGroup;
+
 
   constructor(
     private route: ActivatedRoute,
-    private movieService: MovieService
-  ) { }
+    private movieService: MovieService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((paramMap: any) => {
@@ -23,6 +26,28 @@ export class MovieComponent implements OnInit {
         this.movie = movie;
       })
     });
+
+    this.synopsisForm = this.formBuilder.group({
+      synopsisTerm: [
+        '', // Default value for the control
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(2)
+        ])
+      ]
+    });
+  }
+
+  public clearSynopsisTerm() {
+    this.synopsisForm.controls.synopsisTerm.setValue('');
+  }  
+
+  public get synopsisTerm(): AbstractControl {
+    return this.synopsisForm.controls.synopsisTerm;
+  }
+
+  public modifySynopsis(): void {
+    console.log('Congrats, you changed the synopsis :)');
   }
 
 }
